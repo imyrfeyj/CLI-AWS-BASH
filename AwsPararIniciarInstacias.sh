fafca46cc728beb6676676fea1f3a13fed32bbf0
imyrfeyj <mauricio.grilo@gmail.com>
2018-05-29 16:00:41 -0300

    case-sensitive aplicado

--- AwsPararIniciarInstacias.sh
+++ AwsPararIniciarInstacias.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 #########################################################################
-# 									#
+# 									                                    #
 # PararIniciarTodos.sh - inicia ou para todas as instancia da aws       #
-#									#
-# Autor: Mauricio Grillo de Moura (https://github.com/imyrfeyj)  	#
-# Data Criação: 29/05/2018						#
-#									#
+#									                                    #
+# Autor: Mauricio Grillo de Moura (https://github.com/imyrfeyj)  	    #
+# Data Criação: 29/05/2018					                         	#
+#									                                    #
 # Descrição: Script inicia ou para todas as instancias de uma conta AWS #
-#	     que estejam em um estado, este .SH permite que o usuario   #
+#	     que estejam em um estado, este .SH permite que o usuario       #
 #            escolha o estado das instancias que serão afetadas.        #
-#	    				 				#
+#	    				 				                                #
 # Exemplo de uso: ./ParaIniciarTodos                                    #
-#                                                                       #
-# Observações: todas as perguntas devem ser respondidas em letras       #
-#	       minusculas 				        	#
-#									#
+#		                                       							#
 #########################################################################
 
 #Variaveis 
